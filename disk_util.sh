9838b1428f61fa2100900e856c93ba9a2358e7d7
Jack Kolokasis <kolokasis@ics.forth.gr>
2022-09-30 11:05:49 +0300

    [fix] Remove absolute paths

--- disk_util.sh
+++ disk_util.sh
@@ -1,91 +1,88 @@
 #!/usr/bin/env bash
 
-# Remember to set the debices
-#DEVICES=( nvme2n1p1 )
-
 # Check for the input arguments
 while getopts "b:a:s:r:d:h" opt
 do
-    case "${opt}" in
-        b)
-            DSTATS_BEFORE="${OPTARG}"
-            ;;
-        a)
-            DSTATS_AFTER="${OPTARG}"
-            ;;
-        s)
-            IOSTAT="${OPTARG}"
-            ;;
-        r)
-            RESULT_DIR="${OPTARG}"
-            ;;
-        d)
-			DEVICES+=("${OPTARG}")
-            ;;
-        h)
-            usage
-            ;;
-        *)
-            usage
-            ;;
-    esac
+  case "${opt}" in
+    b)
+      DSTATS_BEFORE="${OPTARG}"
+      ;;
+    a)
+      DSTATS_AFTER="${OPTARG}"
+      ;;
+    s)
+      IOSTAT="${OPTARG}"
+      ;;
+    r)
+      RESULT_DIR="${OPTARG}"
+      ;;
+    d)
+      DEVICES+=("${OPTARG}")
+      ;;
+    h)
+      usage
+      ;;
+    *)
+      usage
+      ;;
+  esac
 done
 
 for dev in "${DEVICES[@]}"
 do
-	R_SEC_BEFORE=$(grep $dev ${DSTATS_BEFORE} | awk '{ print $6 }')
-	W_SEC_BEFORE=$(grep $dev ${DSTATS_BEFORE} | awk '{ print $10 }')
+  R_SEC_BEFORE=$(grep $dev ${DSTATS_BEFORE} | awk '{ print $6 }')
+  W_SEC_BEFORE=$(grep $dev ${DSTATS_BEFORE} | awk '{ print $10 }')
 
-	R_SEC_AFTER=$(grep $dev ${DSTATS_AFTER} | awk '{ print $6 }')
-	W_SEC_AFTER=$(grep $dev ${DSTATS_AFTER} | awk '{ print $10 }')
+  R_SEC_AFTER=$(grep $dev ${DSTATS_AFTER} | awk '{ print $6 }')
+  W_SEC_AFTER=$(grep $dev ${DSTATS_AFTER} | awk '{ print $10 }')
 
-	DIFF_SEC_READ=$(expr ${R_SEC_AFTER} - ${R_SEC_BEFORE})
-	DIFF_SEC_WRITE=$(expr ${W_SEC_AFTER} - ${W_SEC_BEFORE})
+  DIFF_SEC_READ=$(expr ${R_SEC_AFTER} - ${R_SEC_BEFORE})
+  DIFF_SEC_WRITE=$(expr ${W_SEC_AFTER} - ${W_SEC_BEFORE})
 
-	DIFF_BYTES_READ=$(expr ${DIFF_SEC_READ} \* 512)
-	DIFF_BYTES_WRITE=$(expr ${DIFF_SEC_WRITE} \* 512)
+  DIFF_BYTES_READ=$(expr ${DIFF_SEC_READ} \* 512)
+  DIFF_BYTES_WRITE=$(expr ${DIFF_SEC_WRITE} \* 512)
 
-	DIFF_KB_READ=$(expr ${DIFF_BYTES_READ} / 1024)
-	DIFF_KB_WRITE=$(expr ${DIFF_BYTES_WRITE} / 1024)
+  DIFF_KB_READ=$(expr ${DIFF_BYTES_READ} / 1024)
+  DIFF_KB_WRITE=$(expr ${DIFF_BYTES_WRITE} / 1024)
 
-	DIFF_MB_READ=$(expr ${DIFF_BYTES_READ} / 1024 / 1024)
-	DIFF_MB_WRITE=$(expr ${DIFF_BYTES_WRITE} / 1024 / 1024)
+  DIFF_MB_READ=$(expr ${DIFF_BYTES_READ} / 1024 / 1024)
+  DIFF_MB_WRITE=$(expr ${DIFF_BYTES_WRITE} / 1024 / 1024)
 
-	DIFF_GB_READ=$(expr ${DIFF_BYTES_READ} / 1024 / 1024 / 1024)
-	DIFF_GB_WRITE=$(expr ${DIFF_BYTES_WRITE} / 1024 / 1024 / 1024)
+  DIFF_GB_READ=$(expr ${DIFF_BYTES_READ} / 1024 / 1024 / 1024)
+  DIFF_GB_WRITE=$(expr ${DIFF_BYTES_WRITE} / 1024 / 1024 / 1024)
 
-	echo "${dev},TOTAL_READS(KB),${DIFF_KB_READ}" >> ${RESULT_DIR}/diskstat.csv
-	echo "${dev},TOTAL_WRITES(KB),${DIFF_KB_WRITE}" >> ${RESULT_DIR}/diskstat.csv
-	echo "${dev},TOTAL_READS(MB),${DIFF_MB_READ}" >> ${RESULT_DIR}/diskstat.csv
-	echo "${dev},TOTAL_WRITES(MB),${DIFF_MB_WRITE}" >> ${RESULT_DIR}/diskstat.csv
-	echo "${dev},TOTAL_READS(GB),${DIFF_GB_READ}" >> ${RESULT_DIR}/diskstat.csv
-	echo "${dev},TOTAL_WRITES(GB),${DIFF_GB_WRITE}" >> ${RESULT_DIR}/diskstat.csv
-	# Add two blank lines
-	echo "," >> ${RESULT_DIR}/diskstat.csv
+  echo "${dev},TOTAL_READS(KB),${DIFF_KB_READ}" >> ${RESULT_DIR}/diskstat.csv
+  echo "${dev},TOTAL_WRITES(KB),${DIFF_KB_WRITE}" >> ${RESULT_DIR}/diskstat.csv
+  echo "${dev},TOTAL_READS(MB),${DIFF_MB_READ}" >> ${RESULT_DIR}/diskstat.csv
+  echo "${dev},TOTAL_WRITES(MB),${DIFF_MB_WRITE}" >> ${RESULT_DIR}/diskstat.csv
+  echo "${dev},TOTAL_READS(GB),${DIFF_GB_READ}" >> ${RESULT_DIR}/diskstat.csv
+  echo "${dev},TOTAL_WRITES(GB),${DIFF_GB_WRITE}" >> ${RESULT_DIR}/diskstat.csv
+  # Add two blank lines
+  echo "," >> ${RESULT_DIR}/diskstat.csv
 done
 
 for dev in "${DEVICES[@]}"
 do
-	# remove suffix starting with "p"	
-	RM_SF_DEV_NAME=${dev%p*}
+  # remove suffix starting with "p"	
+  RM_SF_DEV_NAME=${dev%p*}
 
-	cat ${IOSTAT} | grep $RM_SF_DEV_NAME > iostat.out.txt
+  cat ${IOSTAT} | grep $RM_SF_DEV_NAME > iostat.out.txt
 
-	R_MB_PER_SEC=$(cat iostat.out.txt | awk '{print $6 }' | awk '{ sum += $1; n++ } END { if (n > 0) print (sum / n); }')
-	W_MB_PER_SEC=$(cat iostat.out.txt | awk '{print $7 }' | awk '{ sum += $1; n++ } END { if (n > 0) print (sum / n); }')
-	AVG_RQ_SZ=$(cat iostat.out.txt | awk '{print $8 }' | awk '{ sum += $1; n++ } END { if (n > 0) print (sum / n); }')
-	AVG_QU_SZ=$(cat iostat.out.txt | awk '{print $9 }' | awk '{ sum += $1; n++ } END { if (n > 0) print (sum / n); }')
-	DEV_UTIL=$(cat iostat.out.txt | awk '{print $14 }' | awk '{ sum += $1; n++ } END { if (n > 0) print (sum / n); }')
+  R_MB_PER_SEC=$(cat iostat.out.txt | awk '{print $6 }' | awk '{ sum += $1; n++ } END { if (n > 0) print (sum / n); }')
+  W_MB_PER_SEC=$(cat iostat.out.txt | awk '{print $7 }' | awk '{ sum += $1; n++ } END { if (n > 0) print (sum / n); }')
+  AVG_RQ_SZ=$(cat iostat.out.txt | awk '{print $8 }' | awk '{ sum += $1; n++ } END { if (n > 0) print (sum / n); }')
+  AVG_QU_SZ=$(cat iostat.out.txt | awk '{print $9 }' | awk '{ sum += $1; n++ } END { if (n > 0) print (sum / n); }')
+  DEV_UTIL=$(cat iostat.out.txt | awk '{print $14 }' | awk '{ sum += $1; n++ } END { if (n > 0) print (sum / n); }')
 
-	echo "${dev},rMB/s,${R_MB_PER_SEC}" >> ${RESULT_DIR}/diskstat.csv
-	echo "${dev},wMB/s,${W_MB_PER_SEC}" >> ${RESULT_DIR}/diskstat.csv
-	echo "${dev},AVGRQ-SZ,${AVG_RQ_SZ}" >> ${RESULT_DIR}/diskstat.csv
-	echo "${dev},AVGQU-SZ,${AVG_QU_SZ}" >> ${RESULT_DIR}/diskstat.csv
-	echo "${dev},DEV_UTIL,${DEV_UTIL}"  >> ${RESULT_DIR}/diskstat.csv
-	echo "," >> ${RESULT_DIR}/diskstat.csv
+  echo "${dev},rMB/s,${R_MB_PER_SEC}" >> ${RESULT_DIR}/diskstat.csv
+  echo "${dev},wMB/s,${W_MB_PER_SEC}" >> ${RESULT_DIR}/diskstat.csv
+  echo "${dev},AVGRQ-SZ,${AVG_RQ_SZ}" >> ${RESULT_DIR}/diskstat.csv
+  echo "${dev},AVGQU-SZ,${AVG_QU_SZ}" >> ${RESULT_DIR}/diskstat.csv
+  echo "${dev},DEV_UTIL,${DEV_UTIL}"  >> ${RESULT_DIR}/diskstat.csv
+  echo "," >> ${RESULT_DIR}/diskstat.csv
 
-	rm iostat.out.txt
+  rm iostat.out.txt
 done
 
 # TODO Change script to plot the statistics for all devices
-~/system_util/plot_iostat.py -i ${IOSTAT} -o ${RESULT_DIR}/plots -s ${DEVICES[0]}
+"$(pwd)"/system_util/plot_iostat.py -i ${IOSTAT} -o ${RESULT_DIR}/plots -s ${DEVICES[0]}
--- extract-data.sh
+++ extract-data.sh
@@ -20,48 +20,51 @@ while getopts "r:d:h" opt
 do
     case "${opt}" in
         r)
-            RESULT_DIR="${OPTARG}"
-            ;;
+          RESULT_DIR="${OPTARG}"
+          ;;
         d)
-			DEVICES+=(-d "${OPTARG}")
-            ;;
+          DEVICES+=(-d "${OPTARG}")
+          ;;
         h)
-            usage
-            ;;
+          usage
+          ;;
         *)
-            usage
-            ;;
-    esac
-done
+          usage
+          ;;
+      esac
+    done
+
 
 # Calculate averages for user, systet, iowait, idle and cpu utilization
-USR_UTIL=$(grep all ${RESULT_DIR}/mpstat-* | head -n -1 | awk '{ print $4 }' \
+USR_UTIL=$(grep all "${RESULT_DIR}"/mpstat-* | head -n -1 | awk '{ print $4 }' \
 	| grep -v "0,00" | awk -F ',' '{print $1"."$2}' \
 	| awk '{ sum += $1; n++ } END { if (n > 0) print (sum / n); }')
-SYS_UTIL=$(grep all ${RESULT_DIR}/mpstat-* | head -n -1 | awk '{ print $6 }' \
+SYS_UTIL=$(grep all "${RESULT_DIR}"/mpstat-* | head -n -1 | awk '{ print $6 }' \
 	| grep -v "0,00" | awk -F ',' '{print $1"."$2}' \
 	| awk '{ sum += $1; n++ } END { if (n > 0) print (sum / n); }')
-IOW_UTIL=$(grep all ${RESULT_DIR}/mpstat-* | head -n -1 | awk '{ print $7 }' \
+IOW_UTIL=$(grep all "${RESULT_DIR}"/mpstat-* | head -n -1 | awk '{ print $7 }' \
 	| grep -v "0,00" | awk -F ',' '{print $1"."$2}' \
 	| awk '{ sum += $1; n++ } END { if (n > 0) print (sum / n); }')
-IDL_UTIL=$(grep all ${RESULT_DIR}/mpstat-* | head -n -1 | awk '{ print $13 }'\
+IDL_UTIL=$(grep all "${RESULT_DIR}"/mpstat-* | head -n -1 | awk '{ print $13 }'\
 	| grep -v "0,00" | awk -F ',' '{print $1"."$2}' \
 	| awk '{ sum += $1; n++ } END { if (n > 0) print (sum / n); }')
 
-CPU_UTIL=$(grep all ${RESULT_DIR}/mpstat-* | head -n -1 | awk '{ print $13 }' \
+CPU_UTIL=$(grep all "${RESULT_DIR}"/mpstat-* | head -n -1 | awk '{ print $13 }' \
 	| grep -v "0,00" | awk -F ',' '{print $1"."$2}' \
 	| awk '{ sum += $1; n++ } END { if (n > 0) print 100 - (sum / n); }')
 
-echo "USR_UTIL(%),${USR_UTIL}" > ${RESULT_DIR}/system.csv
-echo "SYS_UTIL(%),${SYS_UTIL}" >> ${RESULT_DIR}/system.csv
-echo "IOW_UTIL(%),${IOW_UTIL}" >> ${RESULT_DIR}/system.csv
-echo "IDL_UTIL(%),${IDL_UTIL}" >> ${RESULT_DIR}/system.csv
-echo "CPU_UTIL(%),${CPU_UTIL}" >> ${RESULT_DIR}/system.csv
+{
+  echo "USR_UTIL(%),${USR_UTIL}" 
+  echo "SYS_UTIL(%),${SYS_UTIL}"
+  echo "IOW_UTIL(%),${IOW_UTIL}"
+  echo "IDL_UTIL(%),${IDL_UTIL}"
+  echo "CPU_UTIL(%),${CPU_UTIL}"
+} >> "${RESULT_DIR}"/system.csv
 
 # Extract the statistics of storage devices utilization
-~/system_util/disk_util.sh \
-	-b ${RESULT_DIR}/diskstats-before-* \
-	-a ${RESULT_DIR}/diskstats-after-* \
-	-s ${RESULT_DIR}/iostat-* \
-	-r ${RESULT_DIR} \
-	${DEVICES[@]}
+"$(pwd)"/system_util/disk_util.sh \
+	-b "${RESULT_DIR}"/diskstats-before-* \
+	-a "${RESULT_DIR}"/diskstats-after-* \
+	-s "${RESULT_DIR}"/iostat-* \
+  -r "${RESULT_DIR}" \
+  "${DEVICES[@]}"
