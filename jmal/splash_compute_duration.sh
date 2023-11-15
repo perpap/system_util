61ce5e222a031945eeca1e65392ac982ba159989
John Malliotakis <johnmallias@protonmail.com>
2023-11-15 14:43:18 +0200

    Add script to get SPLASH workload initialization durations

---
+++ jmal/splash_compute_duration.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Input argument: SPLASH
+# workload output filename (currently covers
+# FMM, FFT, BARNES, LU, RADIX, OCEAN)
+#
+# Output: initialization phase duration
+OUTFILE=$1
+WORKLOAD=`basename $OUTFILE .out`
+
+case $WORKLOAD in
+	BARNES)
+		INIT_DUR=`grep "INITTIME" $OUTFILE | awk '{ print $NF }'`
+		echo $INIT_DUR
+		;;
+	OCEAN)
+		# Fallthrough to FMM as they require the same handling
+		;&
+	RADIX)
+		# Fallthrough to FMM as they require the same handling
+		;&
+	LU)
+		# Fallthrough to FMM as they require the same handling
+		;&
+	FFT)
+		# Fallthrough to FMM as they require the same handling
+		;&
+	FMM)
+		INIT_START=`grep "Start time" $OUTFILE | awk '{ print $NF }'`
+		INIT_END=`grep "Initialization finish time" $OUTFILE | awk '{ print $NF }'`
+		INIT_DUR=$(( $INIT_END - $INIT_START ))
+		echo $INIT_DUR
+		;;
+	*)
+		echo "Unrecognized workload $WORKLOAD"
+		;;
+esac
