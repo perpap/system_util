d6b40f03a017106df15990fe9edde341684ed26a
John Malliotakis <johnmallias@protonmail.com>
2023-11-16 13:08:17 +0200

    Update extract data script to produce stats for computation phase only, update splash duration script to either output initialization or computation duration

--- jmal/extract-data.sh
+++ jmal/extract-data.sh
@@ -1,40 +1,16 @@
 #!/bin/bash
 
-# Data parsing script. Move this, along with helper awk scripts into
-# raw results directory. The script assumes that the results directory has one
-# directory per evaluated configuration, with per workload output files within.
-#
-# The following files are assumed to be in the corresponding result directory for a configuration:
-#	- A <workload>.out file with the output of time -v <workload>, used
-#	  to extract execution time and page faults (in the case of non-xmap
-#	  configurations)
-#	- A <workload>.iostat file with iostat output. The command used for iostat is:
-#	  	iostat -t -x -y ${DEVICE} 1 > ${RESULT_DIR}/${WORKLOAD}.iostat &
-#	- A <workload>.mpstat file with mpstat output. The command used for mpstat is:
-#		mpstat -P ALL 1 > ${RESULT_DIR}/${WORKLOAD}.mpstat &
-#	- A <workload>.xmap file with xmap stats. Stats extracted once workload is finished with:
-#		cat /proc/xmap/xmap_stats > ${RESULT_DIR}/${WORKLOAD}.xmap
-#	- Two files with output from /proc/diskstats, before and after the workload, to calculate
-#	  overall I/O traffic. Therefore run:
-#		 cat /proc/diskstats > ${RESULT_DIR}/${WORKLOAD}_before.diskstats
-#		 time -v <workload>
-#		 cat /proc/diskstats > ${RESULT_DIR}/${WORKLOAD}_after.diskstats
-# Output is one CSV file with parsed stats per workload, one line per configuration within
-# each CSV file
-
 # Workloads
-declare -a workload_arr=("BARNES" "FFT" "FMM" "OCEAN" "LU" "RADIX")
+declare -a workload_arr=("FMM" "FFT" "LU" "RADIX" "OCEAN")
 # Workload file base name
-WORKLOAD=(BARNES FFT FMM OCEAN LU RADIX )
+WORKLOAD=(FMM FFT LU RADIX OCEAN)
 
 # Configurations
 declare -a config_arr=("Swap" "MMAP" "xmap Regular Pages" "xmap Hugepages" "xmap Promotions")
-CONFIG=( swapper_boot_memlim_nothp_nora vanilla_mmap_bootlim_nora xmap_regular xmap_huge_sync xmap_promo )
+CONFIG=(swapper_boot_memlim_nothp_nora vanilla_mmap_bootlim_nora xmap_regular xmap_huge_sync xmap_promo)
 
 BASE_DIR=`pwd`
-
-# CSV output directory
-CSV_DIR=${BASE_DIR}/parsed
+CSV_DIR=${BASE_DIR}/parsed_computation
 
 mkdir -p ${CSV_DIR}
 
@@ -50,23 +26,28 @@ for ((i = 0; i < ${#WORKLOAD[@]}; i++)); do
 	for ((j = 0; j < ${#CONFIG[@]}; j++)); do
 		cd ${CONFIG[$j]}
 
+		# Timeout or error?
+		# Write 0 so that the gnuplot script properly handles it
+		EXEC_TIME=`grep "Elapsed" ${WORKLOAD[$i]}.out | awk ' {print $NF }' | \
+			awk -f ${BASE_DIR}/parse_exec_time.awk | head -n 1`
+		WORKLOAD_TERMINATED=`grep "terminated" ${WORKLOAD[$i]}.out`
+		if [[ -z "$EXEC_TIME" || -n "$WORKLOAD_TERMINATED" ]]; then
+			INIT_TIME="0"
+			EXEC_TIME="0"
+		else
+			INIT_TIME=`${BASE_DIR}/splash_compute_duration.sh ${WORKLOAD[$i]}.out init`
+			EXEC_TIME=`${BASE_DIR}/splash_compute_duration.sh ${WORKLOAD[$i]}.out compute`
+		fi
+
 		# CPU utilization statistics
 		CPUSTATS_FILE=/tmp/`whoami`_${WORKLOAD[$i]}.cpustats
-		awk -f ${BASE_DIR}/mpstat_cpuset.awk ${WORKLOAD[$i]}.mpstat > ${CPUSTATS_FILE}
+		awk -f ${BASE_DIR}/mpstat_cpuset.awk -v skip_intervals=${INIT_TIME} ${WORKLOAD[$i]}.mpstat > ${CPUSTATS_FILE}
 		USR_UTIL=`grep User ${CPUSTATS_FILE} | awk '{ print $NF }'`
 		SYS_UTIL=`grep System ${CPUSTATS_FILE} | awk '{ print $NF }'`
 		IOW_UTIL=`grep IOwait ${CPUSTATS_FILE} | awk '{ print $NF }'`
 		IDL_UTIL=`grep Idle ${CPUSTATS_FILE} | awk '{ print $NF }'`
 		rm -f ${CPUSTATS_FILE}
 
-		EXEC_TIME=`grep "Elapsed" ${WORKLOAD[$i]}.out | awk ' {print $NF }' | \
-			awk -f ${BASE_DIR}/parse_exec_time.awk | head -n 1`
-		WORKLOAD_TERMINATED=`grep "terminated" ${WORKLOAD[$i]}.out`
-		# Timeout or error?
-		# Write 0 so that the gnuplot script properly handles it
-		if [[ -z "$EXEC_TIME" || -n "$WORKLOAD_TERMINATED" ]]; then
-			EXEC_TIME="0"
-		fi
 		if [ -e *${WORKLOAD[$i]}.xmap ]; then
 			FAULT_FILE=${WORKLOAD[$i]}.xmap
 		else
@@ -74,20 +55,22 @@ for ((i = 0; i < ${#WORKLOAD[@]}; i++)); do
 		fi
 		MAJOR_FAULTS=`grep "Major" ${FAULT_FILE} | awk '{print $NF}' | head -n 1`
 		MINOR_FAULTS=`grep "Minor" ${FAULT_FILE} | awk '{print $NF}' | head -n 1`
-		# Concatenate and parse diskstats
-		cat ${WORKLOAD[$i]}_before.diskstats > /tmp/joined_diskstats
-		cat ${WORKLOAD[$i]}_after.diskstats >> /tmp/joined_diskstats
-		READ_TRAFFIC=`awk -f ${BASE_DIR}/diskstats.awk /tmp/joined_diskstats | grep "Read Traffic" | \
-			awk '{print $NF}'`
-		WRITE_TRAFFIC=`awk -f ${BASE_DIR}/diskstats.awk /tmp/joined_diskstats | grep "Write Traffic" | \
-			awk '{print $NF}'`
-		rm -f /tmp/joined_diskstats
 
-		# Produce iostat statistics
-		# I don't currently use the for plots but you could add them
-		# to the CSV output by parsing the output of this awk script
-		# with grep
-		# awk -f iostat_avg.awk ${WORKLOAD[$element]}.iostat
+		# Concatenate and parse diskstats
+		#cat ${WORKLOAD[$i]}_before.diskstats > /tmp/joined_diskstats
+		#cat ${WORKLOAD[$i]}_after.diskstats >> /tmp/joined_diskstats
+		#READ_TRAFFIC=`awk -f ${BASE_DIR}/diskstats.awk /tmp/joined_diskstats | grep "Read Traffic" | \
+		#	awk '{print $NF}'`
+		#WRITE_TRAFFIC=`awk -f ${BASE_DIR}/diskstats.awk /tmp/joined_diskstats | grep "Write Traffic" | \
+		#	awk '{print $NF}'`
+		#rm -f /tmp/joined_diskstats
+
+		# Produce iostat statistics for computation phase
+		IOSTATS_FILE=/tmp/`whoami`_${WORKLOAD[$i]}.iostats
+		awk -f ${BASE_DIR}/iostat_avg.awk -v skip_intervals=${INIT_TIME} ${WORKLOAD[$element]}.iostat > ${IOSTATS_FILE}
+		READ_TRAFFIC=`grep "Read Traffic" ${IOSTATS_FILE} | awk '{ print $NF }'`
+		WRITE_TRAFFIC=`grep "Write Traffic" ${IOSTATS_FILE} | awk '{ print $NF }'`
+		rm -f ${IOSTATS_FILE}
 
 		# If timeout/failure write 0 for faults and I/O traffic
 		# to properly display timeout on gnuplot plots
--- jmal/splash_compute_duration.sh
+++ jmal/splash_compute_duration.sh
@@ -8,30 +8,60 @@
 OUTFILE=$1
 WORKLOAD=`basename $OUTFILE .out`
 
-case $WORKLOAD in
-	BARNES)
-		INIT_DUR=`grep "INITTIME" $OUTFILE | awk '{ print $NF }'`
-		echo $INIT_DUR
-		;;
-	OCEAN)
-		# Fallthrough to FMM as they require the same handling
-		;&
-	RADIX)
-		# Fallthrough to FMM as they require the same handling
-		;&
-	LU)
-		# Fallthrough to FMM as they require the same handling
-		;&
-	FFT)
-		# Fallthrough to FMM as they require the same handling
-		;&
-	FMM)
-		INIT_START=`grep "Start time" $OUTFILE | awk '{ print $NF }'`
-		INIT_END=`grep "Initialization finish time" $OUTFILE | awk '{ print $NF }'`
-		INIT_DUR=$(( $INIT_END - $INIT_START ))
-		echo $INIT_DUR
-		;;
-	*)
-		echo "Unrecognized workload $WORKLOAD"
-		;;
-esac
+if [[ "$2" = "init" ]]; then
+	case $WORKLOAD in
+		BARNES)
+			INIT_DUR=`grep "INITTIME" $OUTFILE | awk '{ print $NF }'`
+			echo $INIT_DUR
+			;;
+		OCEAN)
+			# Fallthrough to FMM as they require the same handling
+			;&
+		RADIX)
+			# Fallthrough to FMM as they require the same handling
+			;&
+		LU)
+			# Fallthrough to FMM as they require the same handling
+			;&
+		FFT)
+			# Fallthrough to FMM as they require the same handling
+			;&
+		FMM)
+			INIT_START=`grep "Start time" $OUTFILE | awk '{ print $NF }'`
+			INIT_END=`grep "Initialization finish time" $OUTFILE | awk '{ print $NF }'`
+			INIT_DUR=$(( $INIT_END - $INIT_START ))
+			echo $INIT_DUR
+			;;
+		*)
+			echo "Unrecognized workload $WORKLOAD"
+			;;
+	esac
+elif [[ "$2" = "compute" ]]; then
+	case $WORKLOAD in
+		BARNES)
+			COMPUTE_DUR=`grep "COMPUTETIME" $OUTFILE | awk '{ print $NF }'`
+			echo $COMPUTE_DUR
+			;;
+		OCEAN)
+			# Fallthrough to FMM as they require the same handling
+			;&
+		RADIX)
+			# Fallthrough to FMM as they require the same handling
+			;&
+		LU)
+			# Fallthrough to FMM as they require the same handling
+			;&
+		FFT)
+			# Fallthrough to FMM as they require the same handling
+			;&
+		FMM)
+			COMPUTE_DUR=`grep "Total time without initialization" $OUTFILE | awk '{ print $NF }'`
+			echo ${COMPUTE_DUR}
+			;;
+		*)
+			echo "Unrecognized workload $WORKLOAD"
+			;;
+	esac
+else
+	echo "Unknown option $2"
+fi
