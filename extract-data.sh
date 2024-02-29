334acd6c8de40d66278f042c42e777b0ebb8f4e3
Periklis Papoutsis <perpap@ampere.cluster.ics.forth.gr>
2024-02-29 14:25:55 +0100

    use a function for parsing arguments

--- extract-data.sh
+++ extract-data.sh
@@ -15,56 +15,56 @@ usage() {
     exit 1
 }
 
-# Check for the input arguments
-while getopts "r:d:h" opt
-do
+# Function to parse input arguments
+parse_arguments() {
+  while getopts "r:d:h" opt; do
     case "${opt}" in
-        r)
-          RESULT_DIR="${OPTARG}"
-          ;;
-        d)
-          DEVICES+=(-d "${OPTARG}")
-          ;;
-        h)
-          usage
-          ;;
-        *)
-          usage
-          ;;
-      esac
-    done
+      r) RESULT_DIR="${OPTARG}" ;;
+      d) DEVICES+=(-d "${OPTARG}") ;;
+      h|*) usage ;;
+    esac
+  done
+}
 
-
-# Calculate averages for user, systet, iowait, idle and cpu utilization
-USR_UTIL=$(grep all "${RESULT_DIR}"/mpstat-* | head -n -1 | awk '{ print $4 }' \
+# Function to calculate averages for user, system, iowait, idle and cpu utilization
+calculate_utilization_averages(){
+  USR_UTIL=$(grep all "${RESULT_DIR}"/mpstat-* | head -n -1 | awk '{ print $4 }' \
 	| grep -v "0,00" | awk -F ',' '{print $1"."$2}' \
 	| awk '{ sum += $1; n++ } END { if (n > 0) print (sum / n); }')
-SYS_UTIL=$(grep all "${RESULT_DIR}"/mpstat-* | head -n -1 | awk '{ print $6 }' \
+  SYS_UTIL=$(grep all "${RESULT_DIR}"/mpstat-* | head -n -1 | awk '{ print $6 }' \
 	| grep -v "0,00" | awk -F ',' '{print $1"."$2}' \
 	| awk '{ sum += $1; n++ } END { if (n > 0) print (sum / n); }')
-IOW_UTIL=$(grep all "${RESULT_DIR}"/mpstat-* | head -n -1 | awk '{ print $7 }' \
+  IOW_UTIL=$(grep all "${RESULT_DIR}"/mpstat-* | head -n -1 | awk '{ print $7 }' \
 	| grep -v "0,00" | awk -F ',' '{print $1"."$2}' \
 	| awk '{ sum += $1; n++ } END { if (n > 0) print (sum / n); }')
-IDL_UTIL=$(grep all "${RESULT_DIR}"/mpstat-* | head -n -1 | awk '{ print $13 }'\
+  IDL_UTIL=$(grep all "${RESULT_DIR}"/mpstat-* | head -n -1 | awk '{ print $13 }'\
 	| grep -v "0,00" | awk -F ',' '{print $1"."$2}' \
 	| awk '{ sum += $1; n++ } END { if (n > 0) print (sum / n); }')
 
-CPU_UTIL=$(grep all "${RESULT_DIR}"/mpstat-* | head -n -1 | awk '{ print $13 }' \
+  CPU_UTIL=$(grep all "${RESULT_DIR}"/mpstat-* | head -n -1 | awk '{ print $13 }' \
 	| grep -v "0,00" | awk -F ',' '{print $1"."$2}' \
 	| awk '{ sum += $1; n++ } END { if (n > 0) print 100 - (sum / n); }')
 
-{
-  echo "USR_UTIL(%),${USR_UTIL}" 
-  echo "SYS_UTIL(%),${SYS_UTIL}"
-  echo "IOW_UTIL(%),${IOW_UTIL}"
-  echo "IDL_UTIL(%),${IDL_UTIL}"
-  echo "CPU_UTIL(%),${CPU_UTIL}"
-} >> "${RESULT_DIR}"/system.csv
+  {
+    echo "USR_UTIL(%),${USR_UTIL}"
+    echo "SYS_UTIL(%),${SYS_UTIL}"
+    echo "IOW_UTIL(%),${IOW_UTIL}"
+    echo "IDL_UTIL(%),${IDL_UTIL}"
+    echo "CPU_UTIL(%),${CPU_UTIL}"
+  } >> "${RESULT_DIR}"/system.csv
+}
 
-# Extract the statistics of storage devices utilization
-"$(pwd)"/system_util/disk_util.sh \
-	-b "${RESULT_DIR}"/diskstats-before-* \
-	-a "${RESULT_DIR}"/diskstats-after-* \
-	-s "${RESULT_DIR}"/iostat-* \
-  -r "${RESULT_DIR}" \
-  "${DEVICES[@]}"
+# Function to extract the statistics of storage devices utilization
+run_disk_util(){
+  "$(pwd)"/system_util/disk_util.sh \
+        -b "${RESULT_DIR}"/diskstats-before-* \
+        -a "${RESULT_DIR}"/diskstats-after-* \
+        -s "${RESULT_DIR}"/iostat-* \
+        -r "${RESULT_DIR}" \
+        "${DEVICES[@]}"
+}
+
+parse_arguments "$@"
+calculate_utilization_averages
+run_disk_util
+
